ff4525da9c35f4f13008c5d8407f752f3c955cf6
HelpMeHolySpirit <mz.piatkowski@gmail.com>
2022-12-07 04:03:41 +0100

    Snake completed Game

--- src/GameFrame.java
+++ src/GameFrame.java
@@ -1,2 +1,14 @@
-public class SnakeFrame {
+import javax.swing.*;
+
+public class GameFrame extends JFrame {
+    GameFrame(){
+        this.add(new GamePanel());
+        this.setTitle("Snake");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(420, 420);
+        this.setResizable(false);
+        this.pack();
+        this.setVisible(true);
+        this.setLocationRelativeTo(null);
+    }
 }
